13a69f68875820a1f4253d87e60b137c63c58157
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2021-11-23 12:04:41 +0900

    use lint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM thinca/vim:latest
 
+# reviewdog
+# ENV REVIEWDOG_VERSION=v0.13.0
+
+# RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
+# RUN apk --update add git && \
+#     rm -rf /var/lib/apt/lists/* && \
+#     rm /var/cache/apk/*
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
+set -e
 
-# Check tag name conflicts
-vim --clean --not-a-term --cmd "try | helptags doc | catch | echo v:exception | cquit | endtry" --cmd quit
+if [ -n "${GITHUB_WORKSPACE}" ] ; then
+  cd "${GITHUB_WORKSPACE}" || exit
+fi
 
-# EOF
+# export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+# setup vim-vimhelplint
+export VIMHELPLINT_PATH="${GITHUB_WORKSPACE}/vim-vimhelplint"
+rm -rf "${VIMHELPLINT_PATH}"
+git clone --depth 1 https://github.com/machakann/vim-vimhelplint "${VIMHELPLINT_PATH}"
+
+find doc \( -name "*.txt" -or -name "*.??x"  \) -type f -print0 | xargs  -0 -I {} \
+vim -esN --cmd "set rtp+=${VIMHELPLINT_PATH}" \
+  -c "filetype plugin on"   \
+  -c "e {}" \
+  -c "verb VimhelpLintEcho" \
+  -c q
+# # shellcheck disable=SC2086
+#     | reviewdog -efm="%f:%l:%c:%trror: %m"                                        \
+#                 -efm="%f:%l:%c:%tarning: %m"                                      \
+#                 -efm="%f:%l:%c:%m"                                                \
+#       -name="${INPUT_TOOL_NAME}"                                                  \
+#       -reporter="${INPUT_REPORTER:-github-pr-review}"                             \
+#       -filter-mode="${INPUT_FILTER_MODE}"                                         \
+#       -fail-on-error="${INPUT_FAIL_ON_ERROR}"                                     \
+#       -level="${INPUT_LEVEL}"                                                     \
+#       ${INPUT_REVIEWDOG_FLAGS}
